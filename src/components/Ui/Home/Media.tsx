846a33a32c5222b732bd9765fa12fabbd6096716
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-09-23 21:57:04 +0100

    changed texr-color

--- src/components/Ui/Home/Media.tsx
+++ src/components/Ui/Home/Media.tsx
@@ -7,11 +7,11 @@ export const Media = () => {
       <div className="flex flex-col text-center">
         <img src={Logo} alt="" className="w-[30%] md:w-[40%]" />
         <div className="text-center pt-8">
-          <p className="flex gap-5 text-text text-2xl">
+          <p className="flex gap-5 text-cardbg text-2xl">
             <img src={Call} alt="" className="" />
             (08) 400 2000
           </p>
-          <p className="flex gap-5 text-text text-2xl pt-4">
+          <p className="flex gap-5 text-cardbg text-2xl pt-4">
             <img src={Email} alt="" />
             coffeeborcelle@gmail.com
           </p>
