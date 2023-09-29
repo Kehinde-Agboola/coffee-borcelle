3f8b2546e8866300eb7914e3984092e1babd3bf8
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-09-29 11:48:10 +0100

    head

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,18 +3,23 @@ import { SiteMap } from "../Ui/Home/SiteMap";
 import { Media } from "../Ui/Home/Media";
 export const Footer = () => {
   return (
-    <main className="bg-[#291600] py-[4rem] px-[2rem] xl:px-0">
-      <section className="max-w-screen-lg mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        <div className="">
-          <Contact />
-        </div>
-        <div className="md:ml-[8rem]">
-          <SiteMap />
-        </div>
-        <div className="social-media">
-          <Media />
-        </div>
-      </section>
+    <main>
+      <div className="bg-[#291600] py-[4rem] px-[2rem] xl:px-0">
+        <section className="max-w-screen-lg mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="">
+            <Contact />
+          </div>
+          <div className="md:ml-[8rem]">
+            <SiteMap />
+          </div>
+          <div className="social-media">
+            <Media />
+          </div>
+        </section>
+      </div>
+      <div className="text-center text-[#000000] bg-[#DBBCA1] p-4">
+        <p>Copyright 2020 All Rights Reserved Company Name.</p>
+      </div>
     </main>
   );
 };
